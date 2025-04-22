a8f8ca6c1a3713693b6f5791e2920309a2e37a04
jaldeonfilho <jaldeonfilho@hotmail.com>
2025-04-22 12:05:06 +0100

    Checkout implementation

--- Services/Implementation/CheckoutService.cs
+++ Services/Implementation/CheckoutService.cs
@@ -1,9 +1,55 @@
 ﻿//ToDoMonday // Create CRUD methods: POST, PUT, GET AND DELETE
+using Monday.Models;
+using Monday.Repository.Interfaces;
 using Monday.Services.Interface;
 
 namespace Monday.Services.Implementation
 {
     public class CheckoutService : ICheckoutService
     {
+        private ICheckoutRepository _checkoutRepository;
+
+        public CheckoutService(ICheckoutRepository checkoutRepository)
+        {
+            _checkoutRepository = checkoutRepository;
+        }
+
+        public async Task<Checkout> AddAsync(Checkout checkout)
+        {
+            await _checkoutRepository.AddAsync(checkout);
+            await _checkoutRepository.SaveAsync();
+            return checkout;
+        }
+
+        public async Task<Checkout?> GetByIdAsync(int id)
+        {
+            return await _checkoutRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Checkout>> GetAllAsync()
+        {
+            return await _checkoutRepository.GetAllAsync();
+        }
+
+        public async Task UpdateAsync(Checkout checkout)
+        {
+            _checkoutRepository.Update(checkout);
+            await _checkoutRepository.SaveAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var checkout = await GetByIdAsync(id);
+            if (checkout != null)
+            {
+                _checkoutRepository.Delete(checkout);
+                await _checkoutRepository.SaveAsync();
+            }
+        }
+
+        public decimal CalculateTotalPrice(List<Product> products)
+        {
+            return products.Sum(product => product.Price * product.Amount);
+        }
     }
 }
--- Services/Interface/ICheckoutService.cs
+++ Services/Interface/ICheckoutService.cs
@@ -1,8 +1,15 @@
 ﻿//ToDoMonday 
+using Monday.Models;
+
 namespace Monday.Services.Interface
 {
     public interface ICheckoutService
     {
-
+        Task<Checkout> AddAsync(Checkout checkout);
+        decimal CalculateTotalPrice(List<Product> products);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<Checkout>> GetAllAsync();
+        Task<Checkout?> GetByIdAsync(int id);
+        Task UpdateAsync(Checkout checkout);
     }
 }
